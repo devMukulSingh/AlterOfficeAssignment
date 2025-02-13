782ca8a16183081c9f2b90dc9f1ced2dfc9b77fe
devMukulSingh <mukulsingh2276@gmail.com>
2025-02-13 11:25:43 +0530

    server and db setup

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import { logger } from 'hono/logger'
+
+const app = new Hono()
+
+app.use("/api/*",cors())
+
+app.use(logger())
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+const port = 3000
+console.log(`Server is running on http://localhost:${port}`)
+
+serve({
+  fetch: app.fetch,
+  port
+})
