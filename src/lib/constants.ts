ec72977157c24d08697b4835f9d241925f103fd7
devMukulSingh <mukulsingh2276@gmail.com>
2025-02-13 16:04:56 +0530

    added generate shortUrl api

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,3 +2,6 @@ import { PrismaClient } from "@prisma/client";
 
 
 export const prisma = new PrismaClient()
+
+
+export const BASE_URL_SERVER =  process.env.NODE_ENV==="production" ? "" :`http://localhost:3000/api`;
--- src/routes/shortenApp.ts
+++ src/routes/shortenApp.ts
@@ -1,21 +1,79 @@
 import { Hono } from "hono";
 import { urlSchema } from "../lib/schema.js";
-
+import namor from "namor";
+import { BASE_URL_SERVER, prisma } from "../lib/constants.js";
+import { Prisma } from "@prisma/client";
 
 const shortenApp = new Hono();
 
+ function generateRandomAlias(){
+    const alias = namor.default.generate({ words: 1 });
+    return alias;
+}
 
-shortenApp.post('/',async(c) => {
 
+shortenApp.post('/:userId', async (c) => {
+
+    const { userId } = c.req.param()
     const body = await c.req.json();
     const parsedBody = urlSchema.safeParse(body);
-    if(!parsedBody.success){
+    if (!parsedBody.success) {
         return c.json({
-            error:parsedBody.error.errors.map(err => err.message)
-        },400)
+            error: parsedBody.error.errors.map(err => err.message)
+        }, 400)
+    }
+    const { longUrl, customAlias, topic } = parsedBody.data
+
+    const existingUrl = await prisma.url.findFirst({
+        where:{
+            longUrl
+        }
+    })
+
+    if (existingUrl){
+        return c.json({
+            msg:"Url Already exists",
+            shortUrl: existingUrl.shortUrl
+        },200)
     }
 
+    let alias = customAlias;
+
+    if (!customAlias || customAlias==="") {
+        alias = generateRandomAlias()
+    }
+
+    alias = alias + Date.now().toString().slice(8)
+    const shortUrl = `${BASE_URL_SERVER}/shorten/${alias}`
     
+    try {
+        await prisma.url.create({
+            data: {
+                customAlias: alias ,
+                longUrl,
+                shortUrl,
+                topic,
+                userId
+            }
+        })
+        return c.json({
+            msg: "Url generated successfully",
+            shortUrl
+        })
+    }
+    catch (e) {
+        if(e instanceof Prisma.PrismaClientKnownRequestError){
+            if(e.code === "P2002"){
+                
+                return c.json({
+                    error:"Alias already exists, try another"
+                },409)
+            }
+        }
+        return c.json({
+            error:"Internal server errror"
+        },500)
+    }
 })
 
 
