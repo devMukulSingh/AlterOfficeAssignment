f49cf353d951bbf28ed0c3335a08abfaa04a4d71
devMukulSingh <mukulsingh2276@gmail.com>
2025-02-16 12:06:12 +0530

    chceking ip on server

--- src/middleware/rateLimiter.ts
+++ src/middleware/rateLimiter.ts
@@ -1,11 +1,22 @@
 import { getConnInfo } from "@hono/node-server/conninfo";
 import { redisClient } from "../lib/redisClient.js";
 import type { Context } from "hono";
-import type {  Next } from "hono/types";
+import type { Next } from "hono/types";
 
 export async function rateLimiter(c: Context, next: Next) {
 
-    const clientIp = getConnInfo(c).remote.address || "192.0.0.1";
+    const ip = c.req.header('')
+    const clientIp = getConnInfo(c).remote.address
+    console.log(c.req.raw.headers.get('x-real-ip'),"real ip");
+    console.log(c.req.raw.headers.get('c.env.ip'), "c.env.ip"); 
+   console.log(c.req.raw.headers.get('x-forwarded-for'),"x-forwaded-for");
+    console.log(c.req.header());
+    if (!clientIp) {
+        return c.json({
+            error: "Cannot get client IP"
+        }, 400)
+    }
+    console.log({ clientIp });
     const requests = await redisClient.incr(clientIp)
 
     let ttl;
